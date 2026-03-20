65027eec3be6c9dd7bf68f0434c65dac879227af
devmaxtest <djedoumaurice@gmail.com>
2026-03-20 14:29:35 +0100

    Initial commit: Scaffold React+Vite app structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Tetris Max</h1>
+    </div>
+  );
+};
+
+export default App;
